7fc996ec8942c5e0158517734615a62a838d242e
Nicholas Holloway <nholloway91@gmail.com>
2017-10-27 08:35:54 +0100

    first ten clubs backup data, npm init, assert imported, database spec file set up

---
+++ seed_data.js
@@ -0,0 +1,96 @@
+use team_extra_data;
+
+db.dropDatabase
+
+db.teams.insert([
+  {
+    name: "AFC Bournemouth",
+    stadiumPicture: "https://footballtripper.com/wp-content/uploads/2014/08/AFC-Bournemouth-dean-court.jpg",
+    latLng: [
+      50.7352306,
+      -1.8382791
+    ]
+    ticketLink: "https://www.afcb.co.uk/tickets"
+  },
+  {
+    name: "Arsenal FC",
+    stadiumPicture: "https://footballtripper.com/wp-content/uploads/2014/08/arsenal-emirates-stadium-aerial.jpg",
+    latLng: [
+      51.5548885,
+      -0.108438
+    ]
+    ticketLink: "https://www.arsenal.com/tickets"
+  },
+  {
+    name: "Brighton & Hove Albion",
+    stadiumPicture: "https://footballtripper.com/wp-content/uploads/2015/02/amex-stadium-cover.jpg",
+    latLng: [
+      50.8615651,
+      -0.0837163
+    ]
+    ticketLink: "https://www.seagullstickets.com/"
+  },
+  {
+    name: "Burnley FC",
+    stadiumPicture: "https://footballtripper.com/wp-content/uploads/2015/02/turf-moor-cover.jpg",
+    latLng: [
+      53.7890244,
+      -2.2301736
+    ]
+    ticketLink: "https://www.burnleyfootballclub.com/tickets/"
+  },
+  {
+    name: "Chelsea FC",
+    stadiumPicture: "https://footballtripper.com/wp-content/uploads/2015/02/stamford-bridge-cover.jpg",
+    latLng: [
+      51.481663,
+      -0.1909565
+    ]
+    ticketLink: "http://www.chelseafc.com/tickets-membership/tickets-home.html"
+  },
+  {
+    name: "Crystal Palace FC",
+    stadiumPicture: "https://footballtripper.com/wp-content/uploads/2015/02/selhurst-park-cover.jpg",
+    latLng: [
+      51.3983261,
+      -0.0865955
+    ]
+    ticketLink: "https://www.cpfc.co.uk/tickets/"
+  },
+  {
+    name: "Everton FC",
+    stadiumPicture: "https://footballtripper.com/wp-content/uploads/2015/02/goodison-park-cover.jpg",
+    latLng: [
+      53.438787,
+      -2.9663193
+    ]
+    ticketLink: "http://www.evertonfc.com/ticket-news"
+  },
+  {
+    name: "Huddersfield Town",
+    stadiumPicture: "https://footballtripper.com/wp-content/uploads/2014/08/john-smiths-stadium-huddersfield.jpg",
+    latLng: [
+      53.6542822,
+      -1.7682517
+    ]
+    ticketLink: "https://www.htafc.com/tickets/"
+  },
+  {
+    name: "Leicester City FC",
+    stadiumPicture: "https://footballtripper.com/wp-content/uploads/2014/08/leicester-king-power-stadium.jpg",
+    latLng: [
+      52.6203662,
+      -1.1421895
+    ]
+    ticketLink: "https://tickets.lcfc.com/default.aspx"
+  },
+  {
+    name: "Liverpool FC",
+    stadiumPicture: "https://footballtripper.com/wp-content/uploads/2014/08/anfield-stadium-guide.jpg",
+    latLng: [
+      53.4308294,
+      -2.96083
+    ]
+    ticketLink: "http://www.liverpoolfc.com/tickets/tickets-availability"
+  },
+])
---
+++ specs/database_spec.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+
+describe('Database', function() {
+
+
+  it('should contain 20 objects', function () {
+  assert.strictEqual();
+  });
+
+  it('should be able to get on item by club name', function () {
+  assert.strictEqual();
+  });
+
+  it('should have a latlng for stadium location', function () {
+  assert.strictEqual();
+  });
+
+})
