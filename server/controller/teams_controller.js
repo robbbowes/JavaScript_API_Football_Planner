8f8dba1d3d14a33cbf599a6848a1b28d0a0a8797
Nicholas Holloway <nholloway91@gmail.com>
2017-10-27 11:49:43 +0100

    can access database data through server

---
+++ server/controller/teams_controller.js
@@ -0,0 +1,11 @@
+var clubQuery = require("../db/clubQuery.js");
+var express = require("express");
+var clubRouter = express.Router();
+
+clubRouter.get("/", function (req, res) {
+
+  clubQuery.all(function (teams) {
+    res.json(teams)
+  })
+})
+module.exports = clubRouter;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-var express = require
+var express = require("express");
+var app = express();
+
+var club_router = require("./controller/teams_controller.js");
+
+app.use("/api/clubExtras", club_router);
+
+app.listen(3000, function () {
+  console.log("App running");
+})
