3b049db0c67425f1720261132fa486591c20e09b
Nicholas Holloway <nholloway91@gmail.com>
2017-10-27 10:31:57 +0100

    20 clubs seeded to database

--- seed_data.js
+++ seed_data.js
@@ -1,6 +1,6 @@
 use team_extra_data;
 
-db.dropDatabase
+db.dropDatabase()
 
 db.teams.insert([
   {
@@ -9,7 +9,7 @@ db.teams.insert([
     latLng: [
       50.7352306,
       -1.8382791
-    ]
+    ],
     ticketLink: "https://www.afcb.co.uk/tickets"
   },
   {
@@ -18,7 +18,7 @@ db.teams.insert([
     latLng: [
       51.5548885,
       -0.108438
-    ]
+    ],
     ticketLink: "https://www.arsenal.com/tickets"
   },
   {
@@ -27,7 +27,7 @@ db.teams.insert([
     latLng: [
       50.8615651,
       -0.0837163
-    ]
+    ],
     ticketLink: "https://www.seagullstickets.com/"
   },
   {
@@ -36,7 +36,7 @@ db.teams.insert([
     latLng: [
       53.7890244,
       -2.2301736
-    ]
+    ],
     ticketLink: "https://www.burnleyfootballclub.com/tickets/"
   },
   {
@@ -45,7 +45,7 @@ db.teams.insert([
     latLng: [
       51.481663,
       -0.1909565
-    ]
+    ],
     ticketLink: "http://www.chelseafc.com/tickets-membership/tickets-home.html"
   },
   {
@@ -54,7 +54,7 @@ db.teams.insert([
     latLng: [
       51.3983261,
       -0.0865955
-    ]
+    ],
     ticketLink: "https://www.cpfc.co.uk/tickets/"
   },
   {
@@ -63,7 +63,7 @@ db.teams.insert([
     latLng: [
       53.438787,
       -2.9663193
-    ]
+    ],
     ticketLink: "http://www.evertonfc.com/ticket-news"
   },
   {
@@ -72,7 +72,7 @@ db.teams.insert([
     latLng: [
       53.6542822,
       -1.7682517
-    ]
+    ],
     ticketLink: "https://www.htafc.com/tickets/"
   },
   {
@@ -81,7 +81,7 @@ db.teams.insert([
     latLng: [
       52.6203662,
       -1.1421895
-    ]
+    ],
     ticketLink: "https://tickets.lcfc.com/default.aspx"
   },
   {
@@ -90,7 +90,7 @@ db.teams.insert([
     latLng: [
       53.4308294,
       -2.96083
-    ]
+    ],
     ticketLink: "http://www.liverpoolfc.com/tickets/tickets-availability"
   },
   {
@@ -99,7 +99,7 @@ db.teams.insert([
     latLng: [
       53.4831381,
       -2.2003953
-    ]
+    ],
     ticketLink: "https://tickets.mancity.com/"
   },
   {
@@ -108,7 +108,7 @@ db.teams.insert([
     latLng: [
       53.4630589,
       -2.2913401
-    ]
+    ],
     ticketLink: "https://tickets.mancity.com/"
   },
   {
@@ -117,7 +117,7 @@ db.teams.insert([
     latLng: [
       54.975556,
       -1.621667
-    ]
+    ],
     ticketLink: "https://tickets.nufc.co.uk/"
   },
   {
@@ -126,7 +126,7 @@ db.teams.insert([
     latLng: [
       50.9058218,
       -1.390954
-    ]
+    ],
     ticketLink: "https://southamptonfc.com/tickets/match-tickets"
   },
   {
@@ -135,7 +135,7 @@ db.teams.insert([
     latLng: [
       52.9881046,
       -2.1764298
-    ]
+    ],
     ticketLink: "http://www.stokecityfc.com/tickets"
   },
   {
@@ -144,7 +144,7 @@ db.teams.insert([
     latLng: [
       51.6427499,
       -3.9345855
-    ]
+    ],
     ticketLink: "https://www.eticketing.co.uk/swanstickets"
   },
   {
@@ -153,7 +153,7 @@ db.teams.insert([
     latLng: [
       51.6032123,
       -0.0657389
-    ]
+    ],
     ticketLink: "https://www.eticketing.co.uk/tottenhamhotspur/Events/Index"
   },
   {
@@ -162,7 +162,7 @@ db.teams.insert([
     latLng: [
       51.6499828,
       -0.4016876
-    ]
+    ],
     ticketLink: "https://www.eticketing.co.uk/watfordfc/default.aspx"
   },
   {
@@ -171,7 +171,7 @@ db.teams.insert([
     latLng: [
       52.509038,
       -1.963938
-    ]
+    ],
     ticketLink: "https://www.wba.co.uk/tickets/"
   },
   {
@@ -180,7 +180,9 @@ db.teams.insert([
     latLng: [
       51.5387095,
       -0.0166037
-    ]
+    ],
     ticketLink: "https://www.whufc.com/tickets/match-tickets"
   }
 ])
+
+db.teams.find()
