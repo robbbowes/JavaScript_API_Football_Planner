8f6ff64dc259ac2201226a978bce189f0d973784
Robert James Bowes <up666724@myport.ac.uk>
2017-10-30 19:08:26 +0000

    Display distance, time and address

--- client/views/displayDirections.js
+++ client/views/displayDirections.js
@@ -1,10 +1,39 @@
 var displayDirections = function(routeData) {
-  var timeTaken = routeData.routes[0].legs[0].duration.text
-  var mainDiv = document.getElementById("main-map")
-  var displayTime = document.createElement("p")
-  displayTime.id = "display-journey-time"
-  displayTime.innerText = timeTaken
-  mainDiv.appendChild(displayTime)
+  console.log(routeData);
+  // var mainDiv = document.getElementById("main-div");
+  var instructionsDiv = document.getElementById("instructions-div")
+  // var statsDiv = document.getElementById("stats-div");
+  // var dirInstructions = document.getElementById("directions-div")
+
+  var awayAddress = routeData.routes[0].legs[0].end_address;
+  var timeTaken = routeData.routes[0].legs[0].duration.text;
+  var distance  = routeData.routes[0].legs[0].distance.text;
+  console.log(distance);
+  // var routeInstructions = routeData.routes[0].legs[0].steps;
+  // console.log(routeInstructions);
+
+  var displayAddress = document.createElement("li");
+  var displayTime = document.createElement("li");
+  var displayDistance = document.createElement("li");
+  // var showMeTheWay = document.createElement("ul")
+  console.log(displayAddress);
+
+  displayAddress.id = "display-away-address";
+  displayTime.id = "display-journey-time";
+  displayDistance.id = "display-distance";
+  // displayInstructions = "display-instructions";
+
+  displayAddress.innerText = "Away Stadium address: " + awayAddress;
+  displayTime.innerText = "Estimated travel time: " + timeTaken;
+  displayDistance.innerText = "Distance: " + distance;
+
+
+
+  instructionsDiv.appendChild(displayAddress);
+  instructionsDiv.appendChild(displayTime);
+  instructionsDiv.appendChild(displayDistance);
+
+
 }
 
 module.exports = displayDirections
